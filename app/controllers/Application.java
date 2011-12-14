2e76566a108003089a0652fe82473fe47655e86f
Jeroen Kransen <jeroen@kransen.nl>
2011-12-14 08:04:04 +0100

    init

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.util.List;
 
+import models.Group;
 import models.Resource;
 
 import org.apache.commons.lang.StringUtils;
@@ -14,12 +15,12 @@ public class Application extends Controller {
 	public static final String HTTP = "http://";
 
 	public static void index() {
-		List<Resource> resources = Resource.find("order by title").fetch();
+		List<Resource> resources = Resource.find("order by created desc").fetch();
 		render(resources);
 	}
 
 	public static void addResource(String url) {
-		if (!StringUtils.isBlank(url) && !"null".equals(url)) {
+		if (isText(url)) {
 			Resource resource = new Resource();
 			if (!url.startsWith(HTTP)) {
 				url = HTTP + url;
@@ -34,5 +35,18 @@ public class Application extends Controller {
 		}
 	}
 
+	public static void addGroup(String name) {
+		if (isText(name)) {
+			Group group = new Group();
+			group.name = name;
+			group.save();
+			renderJSON(group);
+		} else {
+			error("Empty url");
+		}
+	}
 
+	private static boolean isText(String text) {
+		return !StringUtils.isBlank(text) && !"null".equals(text);
+	}
 }
