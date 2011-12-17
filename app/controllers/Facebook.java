0e04eab3dce46b3db8f95b10e5d882d9a5f57479
Jeroen Kransen <jeroen@kransen.nl>
2011-12-17 14:01:37 +0100

    init

--- app/controllers/Facebook.java
+++ app/controllers/Facebook.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -17,6 +19,8 @@ public class Facebook extends Controller {
 	public static final String APP_SECRET = "a465e617d556c8ee0a34359129e259cf";
 	public static final String redirectUri = Router.getFullUrl("Facebook.login");
 	
+	private static final Pattern accessTokenBodyMatcher = Pattern.compile("access_token=(.*)&expires=(\\d*)");
+	
 	public static void canvas() {
 		redirect("https://www.facebook.com/dialog/oauth?client_id="+APP_ID+"&redirect_uri="+redirectUri+"&scope=email");
 	}
@@ -26,9 +30,15 @@ public class Facebook extends Controller {
 			Logger.info("code from facebook: %s", code);
 			String accessTokenUrl = "https://graph.facebook.com/oauth/access_token?client_id="+APP_ID+"&redirect_uri="+redirectUri+"&client_secret="+APP_SECRET+"&code="+code;
 			HttpResponse accessTokenResponse = WS.url(accessTokenUrl).get();
-			String accessToken = accessTokenResponse.getString();
-			Logger.info("access token from facebook: %s", accessToken);
-			render(code, accessToken);
+			String accessTokenBody = accessTokenResponse.getString();
+			Logger.info("access token from facebook: %s", accessTokenBody);
+			Matcher matcher = accessTokenBodyMatcher.matcher(accessTokenBody);
+			if (matcher.matches()) {
+				String accessToken = matcher.group(1);
+				String expires = matcher.group(2);
+				render(code, accessToken, expires);
+			}
+//			"https://graph.facebook.com/me?access_token="
 		} else {
 			redirect("Application.index");
 		}
