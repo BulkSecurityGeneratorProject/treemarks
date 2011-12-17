d4f3b4d862abeb0fd7e7cdabaecf538b5b193bd6
Jeroen Kransen <jeroen@kransen.nl>
2011-12-17 13:41:02 +0100

    init

--- app/controllers/Facebook.java
+++ app/controllers/Facebook.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.apache.commons.lang.StringUtils;
 
 import models.Resource;
+import play.Logger;
+import play.libs.WS;
+import play.libs.WS.HttpResponse;
 import play.mvc.Controller;
 import play.mvc.Router;
 
@@ -12,13 +15,23 @@ public class Facebook extends Controller {
 	
 	public static final String APP_ID = "259176644136057";
 	public static final String APP_SECRET = "a465e617d556c8ee0a34359129e259cf";
+	public static final String redirectUri = Router.getFullUrl("Facebook.login");
 	
 	public static void canvas() {
-		redirect("https://www.facebook.com/dialog/oauth?client_id="+APP_ID+"&redirect_uri="+Router.getFullUrl("Facebook.loggedIn"));
+		redirect("https://www.facebook.com/dialog/oauth?client_id="+APP_ID+"&redirect_uri="+redirectUri+"&scope=email");
 	}
 
-	public static void loggedIn() {
-		redirect("Application.index");
+	public static void login(String code) {
+		if (StringUtils.isNotBlank(code)) {
+			Logger.info("code from facebook: %s", code);
+			String accessTokenUrl = "https://graph.facebook.com/oauth/access_token?client_id="+APP_ID+"&redirect_uri="+redirectUri+"&client_secret="+APP_SECRET+"&code="+code;
+			HttpResponse accessTokenResponse = WS.url(accessTokenUrl).get();
+			String accessToken = accessTokenResponse.getString();
+			Logger.info("access token from facebook: %s", accessToken);
+			render(code, accessToken);
+		} else {
+			redirect("Application.index");
+		}
 	}
 	
 	public static void tabread() {
